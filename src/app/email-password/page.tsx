beefead39a11ea562b646c529757550a6e9e87c4
Nick Corfmat <ncorfmat@ucsc.edu>
2026-04-01 11:10:13 -0700

    add login page

---
+++ src/app/email-password/page.tsx
@@ -0,0 +1,13 @@
+import EmailPassword from "./EmailPassword";
+import { createSupabaseServerClient } from "@/lib/server-client";
+
+export default async function LoginPage() {
+  const supabase = await createSupabaseServerClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  console.log( { user })
+
+  return <EmailPassword user={user}/>;
+}
