6c7c6e16634ea3dbefe3ff6b61e7fd868c883899
Nick Corfmat <ncorfmat@ucsc.edu>
2026-04-01 11:07:33 -0700

    add getter function

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -4,3 +4,14 @@ export const supabaseClient = createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 );
+
+export function getEnvironmentVariables() {
+    const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+    const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+    if (!supabaseUrl || !supabaseAnonKey) {
+        throw new Error("Missing Supabase environment variables");
+    }
+
+    return { supabaseUrl, supabaseAnonKey };
+}
