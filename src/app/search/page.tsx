11378643f67803fede504e78daaee038e506fe5a
Nick Corfmat <ncorfmat@ucsc.edu>
2026-04-02 14:37:46 -0700

    formatting update

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -10,15 +10,15 @@ export default async function SearchPage({
   const params = await searchParams;
   const query = params.q || "";
 
-  const { data: products, error } = await supabaseClient
-    .rpc("search_products", { search_query: query });
+  const { data: products, error } = await supabaseClient.rpc(
+    "search_products",
+    { search_query: query }
+  );
 
   return (
     <>
       <div className="p-8 text-black mb-8">
-        <h1 className="text-2xl font-bold mb-6">
-          Results for "{query}"
-        </h1>
+        <h1 className="text-2xl font-bold mb-6">Results for "{query}"</h1>
 
         <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-7 gap-y-8">
           {products?.length ? (
